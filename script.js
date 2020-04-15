028b728b929297f2f88c88c8acc8ce0ba4059d65
Agustina Martin <agustinamartinmorales@gmail.com>
2020-04-15 17:18:46 +1000

    Version con trendings and title

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 ///API FOR GIPHY SEARCH
 
 let APIKEY = "mbaPM0qpbVU4dwmPKI78DrliP7AIQqYV";
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", trend);
 
+
+/// SEARCH
 function init () {
     document.querySelector(".button_grey").addEventListener("click", function () {
-        let url = "https://api.giphy.com/v1/gifs/search?api_key="+APIKEY+"&q=";
+        let url = "https://api.giphy.com/v1/gifs/search?api_key="+APIKEY+"&limit=4"+"&q=";
         let str = document.getElementById("buscador").value.trim();
         url = url.concat(str);
         console.log(url);
@@ -16,9 +18,7 @@ function init () {
                 //DATA //PAGINATION //META
                 console.log(content.data);
                 console.log("Meta:", content.meta);
-
-                /*let img = document.querySelector(".tendencias_box-img");
-                img.src = content.data[0].images.fixed_width.url;*/
+                
                 for (let i = 0; i < content.data.length; i++) {
                     let img = document.querySelectorAll(".sugerencias_box-img")[i];
                     img.src = content.data[i].images.fixed_width.url;
@@ -27,6 +27,29 @@ function init () {
     });
 }; //To stop the page to reload
 
+function trend() {
+        let url = "https://api.giphy.com/v1/gifs/trending?api_key="+APIKEY+"&limit=7"+"&q=";
+        console.log(url);
+
+        fetch(url)
+            .then(response => response.json())
+            .then(content => {
+                //DATA //PAGINATION //META
+                console.log(content.data);
+                console.log("Meta:", content.meta);
+                
+                for (let i = 0; i < content.data.length; i++) {
+                    let img = document.querySelectorAll(".img-tendencias")[i];
+                    img.src = content.data[i].images.fixed_width.url;
+                }
+
+                for (let i = 0; i < content.data.length; i++) {
+                    let p = document.querySelectorAll(".text_hover")[i];
+                    p.innerText = content.data[i].title;
+                }
+
+            });
+};
 
 /* HOVER FUNCTION
 
